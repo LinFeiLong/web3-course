3c2c356034bff70fc38bdfcff36bd7d3b0eeec7f
Fernand Lime <fernand.lime@gmail.com>
2022-11-22 05:34:20 +0100

    add truffle config

--- simple-storage-react/truffle/truffle-config.js
+++ simple-storage-react/truffle/truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 module.exports = {
   contracts_build_directory: "../client/src/contracts",
   networks: {
@@ -5,6 +8,10 @@ module.exports = {
       host: "127.0.0.1", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
+    },
+    goerli:{
+      provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`https://goerli.infura.io/v3/${process.env.INFURA_ID}`})},
+      network_id:5,
     },
   },
 
